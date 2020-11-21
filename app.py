10474a6f18e276094ee1aade4275ac509339631d
Robert Englund <robertenglund980@gmail.com>
2020-11-21 16:34:30 -0600

    initial push

---
+++ app.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return ("Welcome to the home page")
+
+
+@app.route("/api/v1.0/apiname")
+def apiname():
+    return ("Welcome to the api page")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
